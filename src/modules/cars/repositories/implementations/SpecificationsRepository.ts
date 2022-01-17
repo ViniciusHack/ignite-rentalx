47d0866a18a41b94990474f8b7ccbacb11a3cc1d
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-01-17 19:06:34 -0300

    feature: create specification in database and tsyringe

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,23 +1,24 @@
+import { getRepository, Repository } from 'typeorm';
+
 import { Specification } from '../../entities/Specification';
-import { ISpecificationsRepository, ICreateSpecificationDTO } from '../ISpecficitationRepository';
+import { ISpecificationsRepository, ICreateSpecificationDTO } from '../ISpecificationsRepository';
 
 class SpecificationsRepository implements ISpecificationsRepository {
-  private specifications: Specification[];
+  private repository: Repository<Specification>;
   constructor() {
-    this.specifications = [];
+    this.repository = getRepository(Specification);
   }
-  create({ name, description }: ICreateSpecificationDTO): void {
-    const specification = new Specification();
-    Object.assign(specification, {
-      name,
+  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+    console.log(name, description);
+    const specification = this.repository.create({
       description,
-      created_at: new Date(),
+      name,
     });
 
-    this.specifications.push(specification);
+    await this.repository.save(specification);
   }
-  findByName(name: string): Specification | undefined {
-    const specification = this.specifications.find((specification) => specification.name === name);
+  async findByName(name: string): Promise<Specification | undefined> {
+    const specification = await this.repository.findOne({ name });
     return specification;
   }
 }

cf4d04786d403e4a6005c5864843353eeff0428a
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-01-23 04:21:46 -0300

    chore: add avatar column

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 const userRoutes = Router();
 
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 userRoutes.post('/', createUserController.handle);
+
+userRoutes.patch('/avatar', updateUserAvatarController.handle);
 
 export { userRoutes };
